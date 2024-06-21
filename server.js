8aac07d38561a53a835981c6aca4903477addc7a
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-06-21 18:19:00 +1000

    Add route middelware

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 const session = require("express-session");
-
+const ensureLoggedIn = require("./middleware/ensureLoggedIn.js");
+const passGlobalDataToViews = require("./middleware/passGlobalDataToViews.js");
 const authController = require("./controllers/auth.js");
 const listingsController = require("./controllers/listings");
+
 const port = process.env.PORT ? process.env.PORT : "3000";
 
 mongoose.connect(process.env.MONGODB_URI);
@@ -28,6 +30,14 @@ app.use(
   })
 );
 
+app.use(passGlobalDataToViews);
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    user: req.session.user,
+  });
+});
+
 app.get("/", (req, res) => {
   res.render("index.ejs", {
     user: req.session.user,
@@ -43,7 +53,7 @@ app.get("/vip-lounge", (req, res) => {
 });
 
 app.use("/auth", authController);
-app.use("/listings", listingsController);
+app.use("/listings", ensureLoggedIn, listingsController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
